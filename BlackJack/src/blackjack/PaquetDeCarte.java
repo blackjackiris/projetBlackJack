7c88cc1270f59f00bea19b0cb213f8a291a3b97e
vlion <vlion@b108tu3p2.depinfo.touchard.edu>
2014-04-22 16:15:48 +0200

    52cartes

--- BlackJack/src/blackjack/PaquetDeCarte.java
+++ BlackJack/src/blackjack/PaquetDeCarte.java
@@ -6,10 +6,26 @@ package blackjack;
 public class PaquetDeCarte {
  
     public int nbCarte;
-
-
-   
-    Carte [ ] paquet= new Carte[52];
+    Carte [ ] paquet;
+    
+    /**
+     * 
+     */
+    public PaquetDeCarte(){
+        
+        paquet = new Carte[52];
+        byte indice1, indice2;
+        byte nbCarte = 0;
+        
+        for(indice1 = 0; indice1 < 4; indice1 ++){
+            
+            for(indice2 = 0; indice2 < 14; indice2 ++){
+                paquet[nbCarte] = new Carte(indice2, indice1, "" + indice1 + "" + indice2);
+                nbCarte ++;
+            }
+        } 
+    }  
+    
 
     /**
      * Constructeur
@@ -19,6 +35,15 @@ public class PaquetDeCarte {
         this.nbCarte = nbCarte;
     }
 
+    public PaquetDeCarte(PaquetDeCarte pcarte){
+        
+        this.nbCarte = pcarte.nbCarte;        
+        
+    }
+        
+    
+    
+    
 /**
  * Utilisation du random lorsqu'une carte est tirer du paquet
  */
