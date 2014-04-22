0b00b7dafbbffdfbbd380f63eefe9478a968bbf8
croux <croux@b108tu3p1.depinfo.touchard.edu>
2014-04-22 11:52:43 +0200

    Classe Carte

---
+++ BlackJack/src/blackjack/Carte.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package blackjack;
+
+/**
+ *
+ * @author croux
+ */
+public class Carte {
+
+    /**
+     * @param args the command line arguments
+     */
+    
+    private byte valeur;
+    private byte couleur;
+    
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
