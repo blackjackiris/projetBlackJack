8e07b209b7788d9e475b9cade9ae233c0ce189e3
mlouaze <mlouaze@b108tu3p3.depinfo.touchard.edu>
2014-04-22 11:20:39 +0200

    test

--- BlackJack/src/blackjack/BlackJack.java
+++ BlackJack/src/blackjack/BlackJack.java
@@ -17,6 +17,9 @@ public class BlackJack {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        
+        // yolo code 
+        int cd;
     }
     
 }
