7c5537a49cf1686cf96f18607a6b228bf3b52bdd
PC <PC@PC-PC>
2014-05-18 14:23:01 +0200

    Bouton 25 codé

--- BlackJack/src/blackjack/Plateau.java
+++ BlackJack/src/blackjack/Plateau.java
@@ -61,7 +61,7 @@ public class Plateau extends javax.swing.JFrame {
         monDC = getGraphics();
         argent.setText(String.valueOf((j.getArgentTotal())));
         boutonNewPartie.setEnabled(false);
-        boutonTirer.setEnabled(false);
+         boutonTirer.setEnabled(false);
         boutonRester.setEnabled(false);
         boutonDoubler.setEnabled(false);
         boutonAssurance.setEnabled(false);
@@ -81,10 +81,11 @@ public class Plateau extends javax.swing.JFrame {
         valeurMainDonneur = d.main.getValeurMain();
 
         // Si la valeur de la MainJoueur est > à celle de la MainDonneur sans dépasser 21 ou si la valeur de la MainDonneur dépasse 21, alors le joueur gagne
-        if ((valeurMainJoueur > valeurMainDonneur && valeurMainJoueur <= BLACKJACK) || valeurMainDonneur > BLACKJACK) {
+        if((valeurMainJoueur > valeurMainDonneur && valeurMainJoueur <= BLACKJACK) || valeurMainDonneur > BLACKJACK) {
             victoire = true;
-        } // Sinon le joueur perd         
-        else {
+        }
+        // Sinon le joueur perd         
+        else{
             victoire = false;
         }
 
@@ -119,76 +120,81 @@ public class Plateau extends javax.swing.JFrame {
 
         }
     }
-
+    
     //Méthode permettant d'afficher/éffacer les bouttons en fonction des cartes/valeurs
-    public void switchEtatBouttons() {
-
+    public void switchEtatBouttons(){
+        
         Carte carte1, carte2;
         int val;
-
+        
         //Affiche ou efface le boutton Assurance
         carte1 = d.main.getCarteDansMain(0);
-
-        if (carte1.getValeur() == 1 && choixAssurance == false) {
+        
+        if(carte1.getValeur() == 1 && choixAssurance == false){
             boutonAssurance.setEnabled(true);
-        } else {
+        }
+        else{
             boutonAssurance.setEnabled(false);
         }
-
+        
         //Affiche ou efface le boutton Doubler
         carte1 = j.main.getCarteDansMain(0);
         carte2 = j.main.getCarteDansMain(1);
         val = carte1.getValeur() + carte2.getValeur();
-
-        if (val >= 9 && val <= 11) {
+        
+        if(val >= 9 && val <= 11){
             boutonDoubler.setEnabled(true);
-        } else {
+        }
+        else{
             boutonDoubler.setEnabled(false);
         }
-
+        
         //Affiche ou efface le boutton Partager
-        if (carte1.getValeur() == carte2.getValeur()) {
+        if(carte1.getValeur() == carte2.getValeur()){
             boutonPartager.setEnabled(true);
-        } else {
+        }
+        else{
             boutonPartager.setEnabled(true);
         }
     }
-
+    
     /**
      * Méthode pour retourné la carte face cachée du donneur
      */
-    public void retournerSecondeCarteDonneur() {
+    public void retournerSecondeCarteDonneur(){
         Image img;
         Graphics monDC;
         monDC = getGraphics();
-
+        
         String id = "image/" + d.main.getIdSecondeCarte() + ".png";
         img = getImage(id);
         monDC.drawImage(img, 582, 250, null);
         valMainD.setText(String.valueOf((d.main.getValeurMain())));
-
+        
+    
     }
-
-    /**
+    
+        /**
      * Méthode pour retourné la carte face cachée du Joueur
      */
-    public void retournerSecondeCarteJoueur() {
+    public void retournerSecondeCarteJoueur(){
         Image img;
         Graphics monDC;
         monDC = getGraphics();
-
+        
         String id = "image/" + j.main.getIdSecondeCarte() + ".png";
         img = getImage(id);
         monDC.drawImage(img, 582, 600, null);
         valMainD.setText(String.valueOf((j.main.getValeurMain())));
-
+        
+    
     }
 
     /**
      * charge une image à partir d'un fichier utilise ImageIO
      *
      * @param fichImg chaine du nom de l'image
-     * @return img
+     * @return
      */
     private Image getImage(String fichImg) {
         //récup de l'URL du fichier
@@ -399,10 +405,12 @@ public class Plateau extends javax.swing.JFrame {
         indiceJoueur++;
         if (j.main.valeurMainCarteCache() > 21) {
 
+
+            
             victoire();
 
         }
-
+        
         switchEtatBouttons();
 
     }//GEN-LAST:event_boutonTirerActionPerformed
@@ -411,11 +419,11 @@ public class Plateau extends javax.swing.JFrame {
 
         Graphics monDC;
         monDC = getGraphics();
-
+        
         retournerSecondeCarteDonneur();
         retournerSecondeCarteJoueur();
 
-        while (d.main.getValeurMain() < 17) {
+        while(d.main.getValeurMain() < 17){
             d.tirer(jeuDeCartes);
 
             // affiche les cartes de la main du Croupier
@@ -428,10 +436,13 @@ public class Plateau extends javax.swing.JFrame {
         }
 
         valMainD.setText(String.valueOf((d.main.getValeurMain())));
-        valMain.setText(String.valueOf((j.main.getValeurMain())));
+           valMain.setText(String.valueOf((j.main.getValeurMain())));
+           
+           
 
-        victoire();
 
+ victoire();
+ 
 
     }//GEN-LAST:event_boutonResterActionPerformed
 
@@ -450,9 +461,8 @@ public class Plateau extends javax.swing.JFrame {
         bouton10.setEnabled(false);
         bouton100.setEnabled(false);
         bouton5.setEnabled(false);
-        bouton25.setEnabled(false);
         boutonMiser.setEnabled(false);
-
+        
         // atcive le bouton pour commencer a jouer
         boutonDebutPartie.setEnabled(true);
 
@@ -467,7 +477,7 @@ public class Plateau extends javax.swing.JFrame {
 
         // vide la plateau
         Plateau.repaint();
-
+        
         messVictoire.setText("");
 
         // vide la main du joueur
@@ -489,10 +499,10 @@ public class Plateau extends javax.swing.JFrame {
         bouton100.setEnabled(true);
         bouton5.setEnabled(true);
         boutonMiser.setEnabled(true);
-
-        //Désactive le bouton jouer pour oblgier le joueur a miser
+        
+           //Désactive le bouton jouer pour oblgier le joueur a miser
         boutonDebutPartie.setEnabled(false);
-
+ 
 
     }//GEN-LAST:event_boutonNewPartieActionPerformed
 
@@ -506,69 +516,68 @@ public class Plateau extends javax.swing.JFrame {
 
     private void boutonPartagerActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_boutonPartagerActionPerformed
         // TODO add your handling code here:
-
-
+        
+        
     }//GEN-LAST:event_boutonPartagerActionPerformed
 
     private void boutonDebutPartieActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_boutonDebutPartieActionPerformed
         // TODO add your handling code here:
-
+        
         String nomCarte;
         Image img;
         Graphics monDC;
         monDC = getGraphics();
-
+        
         //Efface le bouton de début de partie
         boutonDebutPartie.setEnabled(false);
-
+        
         //Affiche les boutons de jeu
         boutonTirer.setEnabled(true);
         boutonRester.setEnabled(true);
         boutonDoubler.setEnabled(true);
-
+        
         //Indices Joueur et Donneur remis à zero
         indiceJoueur = 0;
         indiceDonneur = 0;
-
+        
         //Le donneur tire une carte face visible
         d.tirer(jeuDeCartes);
         nomCarte = "image/" + d.main.getIdCarteMain() + ".png";
         img = getImage(nomCarte);
         monDC.drawImage(img, 550 + DECALAGEGAUCHE * (indiceDonneur + 1), 250, null);
         valMainD.setText(String.valueOf((d.main.getValeurMain())));
-        indiceDonneur++;
-
+        indiceDonneur ++;
+        
         //Le joueur tire une carte face visible
         j.tirer(jeuDeCartes);
         nomCarte = "image/" + j.main.getIdCarteMain() + ".png";
         img = getImage(nomCarte);
         monDC.drawImage(img, 550 + DECALAGEGAUCHE * (indiceJoueur + 1), 600, null);
         valMain.setText(String.valueOf((j.main.getValeurMain())));
-        indiceJoueur++;
-
+        indiceJoueur ++;
+        
         //Le donneur tire une carte face cachée
         d.tirer(jeuDeCartes);
-        String blank = "image/blank.png";
+       String blank = "image/blank.png";
         img = getImage(blank);
         monDC.drawImage(img, 550 + DECALAGEGAUCHE * (indiceDonneur + 1), 250, null);
         //Ici, l'affichage de la valeur de la main du donneur n'est pas mis à jour, pour garder la valeur de la carte face cachée secrete
-        indiceDonneur++;
-
+        indiceDonneur ++;
+        
         //Le joueur tire une carte face cachéé
         j.tirer(jeuDeCartes);
-
-        img = getImage(blank);
+       
+         img = getImage(blank);
         monDC.drawImage(img, 550 + DECALAGEGAUCHE * (indiceJoueur + 1), 600, null);
-        //  valMain.setText(String.valueOf((j.main.getValeurMain())));
-        indiceJoueur++;
-
+      //  valMain.setText(String.valueOf((j.main.getValeurMain())));
+        indiceJoueur ++;
+        
     }//GEN-LAST:event_boutonDebutPartieActionPerformed
 
     private void bouton25ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_bouton25ActionPerformed
-
+          //Augmente la mise de 25
         miseJoueur = miseJoueur + 25;
         valeurMise.setText(String.valueOf(miseJoueur));
-
     }//GEN-LAST:event_bouton25ActionPerformed
 
     /**
